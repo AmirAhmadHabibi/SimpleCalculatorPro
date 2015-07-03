2c4336825269288dbff028eba3870379187e2e5d
Amir Ahmad Habibi <amirahmadhabibi@gmail.com>
2015-07-04 01:12:20 +0430

    Connected widgets to handlers

--- SCP.cpp
+++ SCP.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+
 static void click(GtkWidget *widget, gpointer data)
 {
     //TODO
@@ -27,9 +28,43 @@ int main(int argc, char *argv[])
 
     button = gtk_builder_get_object(builder, "num0");
     g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
-
     button = gtk_builder_get_object(builder, "num1");
-    g_signal_connect (button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "num2");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "num3");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "num4");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "num5");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "num6");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "num7");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "num8");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "num9");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+
+    button = gtk_builder_get_object(builder, "point");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "result");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "add");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "sub");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "mul");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "div");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "sqr");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "toggleNP");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
+    button = gtk_builder_get_object(builder, "clr");
+    g_signal_connect (button, "clicked", G_CALLBACK(click), NULL);
 
     //TODO
 
